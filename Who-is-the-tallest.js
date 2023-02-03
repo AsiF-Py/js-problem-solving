b08c8b8ba27c4e1d490670c88d5a63cf421f7618
AsiF-Py <asif.mannanuddin@gmail.com>
2023-02-03 15:22:24 +0600

    Who is the tallest

---
+++ Who-is-the-tallest.js
@@ -0,0 +1,12 @@
+function tallestFriend(height){
+    let tallest = height[0];
+    for(var i = 0; i < height.length;i++){
+       let element = height[i]
+       if (element > tallest){
+        tallest = element
+       }
+    }
+    return tallest;
+}
+let heightList = [20,30,50,40,60]
+console.log(tallestFriend(heightList))
