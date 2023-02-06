f4b08831c0d210d020b2e6cc60102bb934520012
AsiF-Py <asif.mannanuddin@gmail.com>
2023-02-06 19:26:26 +0600

    Write foo, bar, foobar if divisible by 3 or 5 or both

---
+++ Write foo, bar, foobar if divisible by 3 or 5 or both.js
@@ -0,0 +1,17 @@
+function foobar(number) {
+    for (let i = 0; i <= number; i++) {
+        if (i % 3=== 0 && i % 5=== 0) {
+            console.log('foobar');
+        }
+        else if (i % 3 === 0) {
+            console.log('foo');
+        }
+        else if (i % 5 === 0) {
+            console.log('bar');
+        }
+        else {
+            console.log(i);
+        } 
+    }    
+}
+foobar(50)
